32208030411a47e5d1a460e3360637374e7cb199
Chouqe <Switch.ne.lox@gmail.com>
2024-01-17 14:44:32 +0300

    +cards +delete button

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,44 @@
-// @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
+const cardList = document.querySelector(".places__list");
 
-// @todo: DOM узлы
+const createCard = (cardData) => {
+  const cardElem = cardTemplate.cloneNode(true);
 
-// @todo: Функция создания карточки
+  const imageElem = cardElem.querySelector(".card__image");
+  const titleElem = cardElem.querySelector(".card__title");
+  const likeButtonElem = cardElem.querySelector(".card__like-button");
+  const deleteButtonElem = cardElem.querySelector(".card__delete-button");
 
-// @todo: Функция удаления карточки
+  imageElem.src = cardData.link;
+  imageElem.alt = "Живописный пейзаж";
+  titleElem.textContent = cardData.name;
 
-// @todo: Вывести карточки на страницу
+  likeButtonElem.addEventListener("click", (event) => {
+    event.target.classList.toggle("card__like-button_is-active");
+  });
+
+  deleteButtonElem.addEventListener("click", () => {
+    cardElem.remove();
+  });
+
+  return cardElem;
+};
+
+const appendCardToDOM = (cardElem) => {
+  cardList.append(cardElem);
+};
+
+cardList.addEventListener("click", (event) => {
+  const deleteButtonElem = event.target.closest(".card__delete-button");
+  if (deleteButtonElem) {
+    const cardElem = deleteButtonElem.closest(".card");
+    if (cardElem) {
+      cardElem.remove();
+    }
+  }
+});
+
+initialCards.forEach((cardData) => {
+  const cardElem = createCard(cardData);
+  appendCardToDOM(cardElem);
+});
