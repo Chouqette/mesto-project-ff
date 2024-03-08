dfa17197004758fcecf9802ef5816b27aaea03e9
Chouqe <Switch.ne.lox@gmail.com>
2024-03-08 15:12:02 +0300

    +modal-optimisation

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,16 +1,24 @@
-const closePopupByOverlay = (evt) => {
-  if (evt.target.classList.contains("popup")) {
-    const openedPopup = document.querySelector(".popup_is-opened");
-    closeModal(openedPopup);
+const popupList = document.querySelectorAll(".popup");
+
+const closePopupByOverlay = (modal, event) => {
+  const preventClosingElements = ["input"];
+  
+  if (!preventClosingElements.some(tag => event.target.closest(tag))) {
+    closeModal(modal);
   }
 };
+
 const openModal = (modal) => {
   if (modal) {
     modal.classList.add("popup_is-opened", "popup_is-animated");
     document.addEventListener("keydown", closeEsc);
-    modal.addEventListener("click", closePopupByOverlay);
+    modal.addEventListener("click", (event) => closePopupByOverlay(modal, event));
   }
 };
+
+popupList.forEach((popup) => {
+  popup.addEventListener("click", (event) => closePopupByOverlay(popup, event));
+});
 
 const closeModal = (modal) => {
   if (modal) {
